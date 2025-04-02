6485cfe00c666a0fd61be199d22a7434d3a1b34f
tranquan1609 <dienquan5436@gmail.com>
2025-04-02 19:46:01 +0700

    BaiAPI

---
+++ TranVuDienQuan_API/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TranVuDienQuan_API.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>
+        options) : base(options)
+        { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ TranVuDienQuan_API/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace TranVuDienQuan_API.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
